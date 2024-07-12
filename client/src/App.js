c49fbead96bdf0c14c64c71221065f74f9e759fa
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-07-12 13:59:16 -0400

    added navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navigation from './Navigation';
 
 export default function App() {
   return (
     <div className="App">
+      <Navigation />
       Hello
     </div>
   );
