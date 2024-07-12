b1a68a0a583e9f7d04df4b639f37037f2f3c7fd4
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-07-12 15:45:43 -0400

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,8 @@
 import './App.css';
-import fetchGeolocationData from './functions/fetchGeolocationData';
 
 export default function App() {
-  const [geolocationData, setGeolocationData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const ipAddress = 'your_ip_address';
-      const data = await fetchGeolocationData(ipAddress);
-      setGeolocationData(data);
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div>
-      {geolocationData ? (
-        <div>
-          <h2>Geolocation Data</h2>
-          <p>Latitude: {geolocationData.latitude}</p>
-          <p>Longitude: {geolocationData.longitude}</p>
-          <p>City: {geolocationData.city}</p>
-          <p>Region: {geolocationData.region}</p>
-          <p>Country: {geolocationData.country}</p>
-        </div>
-      ) : (
-        <p>Loading geolocation data...</p>
-      )}
     </div>
   );
 };
-
