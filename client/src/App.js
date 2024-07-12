14e0e099273732dc5ee7858cd189f1a4137fa119
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-07-12 14:11:25 -0400

    altered theme

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import Navigation from './Navigation';
 
 export default function App() {
+  const theme = Theme.createFromUAParser();
+
+
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: theme.main }}>
       <Navigation />
       Hello
     </div>
--- client/src/Theme.js
+++ client/src/Theme.js
@@ -27,6 +27,26 @@ class Theme {
         this.isMobile = isMobile;
         this.logo = logo;
     }
+    static createFromUAParser() {
+        let parser = new UAParser();
+        let deviceType = parser.getDevice().type;
+        let isMobile = deviceType === 'mobile';
+        let isDesktop = deviceType === 'desktop';
+        let isTablet = deviceType === 'tablet';
+
+        let primaryColor = '#007bff';
+        let secondaryColor = '#6c757d';
+        let tertiaryColor = '#dee2e6';
+        let mainColor = '#ffffff';
+        let fontRegular = 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap';
+        let fontLink = 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap';
+        let logo = isMobile ? 'https://rchrdlss3.github.io/InnovativeRichLogo/darkLogo.png' : 'https://rchrdlss3.github.io/InnovativeRichLogo/lightLogo.png';
+
+        let font = new Font(fontRegular, fontLink);
+        let theme = new Theme(primaryColor, secondaryColor, tertiaryColor, mainColor, font, isMobile, logo);
+
+        return theme;
+    }
 }
 
 
