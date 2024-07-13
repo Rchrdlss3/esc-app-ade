d0ff48e24489a1e8d10707f1f332d0b72ef27f43
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-07-13 16:24:36 -0400

    Express Implementation

--- server/app.js
+++ server/app.js
@@ -6,18 +6,29 @@ const db = mongoose.connection;
 const app = express()
 const userRoute = require('./routes/users');
 const { getLocation } = require('./helpers/locationFunctions');
+const parser = require('ua-parser-js'); 
+const port = 3001;
 
 async function connect() {
     try {
         await mongoose.connect(process.env.DATABASE_URL)
         console.log("connected")
-    } catch(e){
+    } catch (e) {
         res.status(500).send(internalError(e))
     }
 }
-app.listen(3001)
+
+app.get('/api/device-info', (req, res) => {
+    const userAgent = parser(req.headers['user-agent']);
+    const deviceInfo = {
+        isMobile: userAgent.device.type === 'mobile',
+    };
+    res.json(deviceInfo);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 app.use(express.json())
-app.use('/user',userRoute)
+app.use('/user', userRoute)
 
 // getLocation("134.201.250.155")
 connect() 
