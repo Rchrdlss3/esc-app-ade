49a4a3b21078f10b4ffe134e28dc965a1b8847a5
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2024-07-13 19:01:33 -0400

    globe implemented

--- client/src/components/GlobeComponent.js
+++ client/src/components/GlobeComponent.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import Globe from 'react-globe.gl';
 
-export default function GlobeComponent() {
+export default function GlobeComponent({
+    globeImageUrl = '//unpkg.com/three-globe/example/img/earth-blue-marble.jpg',
+    bumpImageUrl = '//unpkg.com/three-globe/example/img/earth-topology.png',
+    backgroundImageUrl = '//unpkg.com/three-globe/example/img/night-sky.png',
+    pointsData,
+    arcsData,
+    polygonsData,
+}) {
+    const globeEl = useRef();
+
+    if (globeEl.current) {
+        globeEl.current.pointOfView({ lat: 20, lng: -20, altitude: 3 });
+        globeEl.current.controls().autoRotate = true; // Enable rotation
+        globeEl.current.controls().autoRotateSpeed = 0.2; // Set rotation speed
+    }
+
 
     return (
-        <div>
-            Globe Component
+        <div className='globe-container'>
+            <Globe
+                ref={globeEl}
+                globeImageUrl={globeImageUrl}
+                bumpImageUrl={bumpImageUrl}
+                backgroundImageUrl={backgroundImageUrl}
+                pointsData={pointsData}
+                arcsData={arcsData}
+                polygonsData={polygonsData}
+            />
         </div>
     );
-};
+}
